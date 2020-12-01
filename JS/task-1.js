40d209e65760658888e3d3b65bea448b62eaee65
Vladislav Churilov <88vvch88@gmail.com>
2020-12-01 21:55:48 +0200

    jjj

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,10 +1,47 @@
-const logItems = function(array) {
-    // твой код
-  };
-  
+const friends = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
+const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+// const logItems = function(array) {
+//     for (let i= 0; i <= 5; i += 1){
+        
+//         // console.log(i+1);
+//         console.log(`${i +1} - ${friends[i]} `);
+        
+//     }
+//     // for(const friend of friends){
+//     //     console.log (friend);
+//     // }
+//   };
+const logItems = function(arrays) {
+    for (const array of arrays){
+        // console.log(array);
+        console.log(`- ${array} `);
+        // console.log(`${array +1} - ${numbers[array]} `);
+        // let i= 0; i <= 100; i += 1 
+        // console.log(`${i+1} - ${array} `);
+
+        
+    }
+}
+logItems(friends);
+logItems(numbers);
+//   logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+//   logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
   /*
    * Вызовы функции для проверки работоспособности твоей реализации.
    */
-  logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+ 
   
-  logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+//   logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+
+
+// const friends = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
+// const logItems = function(array){
+//    for( let i = 0; i <= 50; i +=5){
+
+//     console.log(i);
+//     console.log(friends[i]);
+//    }
+
+// }
---
+++ JS/task-2.js
@@ -0,0 +1,46 @@
+
+const message = prompt ('текст гравировки');
+let pricePerWord = 0;
+
+const calculateEngravingPrice = function(message, pricePerWord) {
+
+    const words = message.split(' ');
+    console.log(words);
+    // message = prompt ([]);
+    // for( word of words);
+    
+  };
+  calculateEngravingPrice();
+
+// console.log(message.split(' '));
+
+// const valueInputRef = document.querySelector('.js-value');
+// const addValueBtn = document.querySelector('.js-add-value');
+// const calcBtn = document.querySelector('.js-calculate');
+// console.log(valueInputRef)
+// const numbers = [];
+// let total = 0;
+
+
+// addValueBtn.addEventListener('click', function(){
+//     const value = Number(valueInputRef.value);
+//     numbers.push(value);
+//     valueInputRef.value = '';
+// }
+// );
+// calcBtn.addEventListener('click',function(){
+//     for(const number of numbers){
+//         console.log(number)
+//         total += number;
+//     }
+//     console.log('Total:', total);
+// }); 
+
+
+
+
+
+
+
+
+
