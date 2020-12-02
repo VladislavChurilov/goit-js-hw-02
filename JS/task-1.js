ff2ccd6b74ea4b9b366d6964dcfbb20ea172ca6d
Vladislav Churilov <88vvch88@gmail.com>
2020-12-02 21:10:37 +0200

    дз 1

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,47 +1,16 @@
-const friends = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
-const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
-// const logItems = function(array) {
-//     for (let i= 0; i <= 5; i += 1){
-        
-//         // console.log(i+1);
-//         console.log(`${i +1} - ${friends[i]} `);
-        
-//     }
-//     // for(const friend of friends){
-//     //     console.log (friend);
-//     // }
-//   };
-const logItems = function(arrays) {
-    for (const array of arrays){
-        // console.log(array);
-        console.log(`- ${array} `);
-        // console.log(`${array +1} - ${numbers[array]} `);
-        // let i= 0; i <= 100; i += 1 
-        // console.log(`${i+1} - ${array} `);
-
-        
+const logItems = function(array) {
+    for(let i = 0; i < array.length; i+=1){    
+    console.log(`${i+1} - ${array[i]}`);
     }
 }
-logItems(friends);
-logItems(numbers);
-//   logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
-//   logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 
-  /*
-   * Вызовы функции для проверки работоспособности твоей реализации.
-   */
- 
-  
-//   logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 
 
 
-// const friends = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
-// const logItems = function(array){
-//    for( let i = 0; i <= 50; i +=5){
 
-//     console.log(i);
-//     console.log(friends[i]);
-//    }
 
-// }
+
+
+
